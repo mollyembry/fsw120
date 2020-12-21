df7ab692e746cdc3f88aea4e422824a365f4b125
mollyembry <molly.embry@bryanuniversity.edu>
2020-12-21 00:21:21 -0500

    first

---
+++ week1/assignment1/App.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+function App() {
+return (
+    <div>
+        <input type="checkbox" />
+        <p>first checkbox</p>
+
+        <input type="checkbox" />
+        <p>second checkbox</p>
+
+        <input type="checkbox" />
+        <p>third checkbox</p>
+
+        <input type="checkbox" />
+        <p>fourth checkbox</p>
+
+    </div>
+)
+}
+export default App
